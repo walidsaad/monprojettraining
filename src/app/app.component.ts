7ad53af5cd53a652be7ac8d7626fbed9eca43d18
Your Name <you@example.com>
2020-05-15 18:04:40 +0200

    add labs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trainig-app';
+//session_name="Formation Mobile";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { SessionItemComponent } from './session-item/session-item.component';
+import { SessionItemListComponent } from './session-item-list/session-item-list.component';
+import { InscriptiondisabledDirective } from './inscriptiondisabled.directive';
+import {RouterModule, Routes} from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+const appRoutes: Routes = [
+  {
+    path: 'list',
+    component: SessionItemListComponent},
+  {
+    path: 'admin',
+    loadChildren: './admin/admin.module#AdminModule'
+  },
+   { path: '',   redirectTo: '/list', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SessionItemComponent,
+    SessionItemListComponent,
+    InscriptiondisabledDirective,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot( appRoutes, { enableTracing: true } ),
     BrowserModule
   ],
   providers: [],
